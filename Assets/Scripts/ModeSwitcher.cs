9425ec8a0a772c5e17d13fce458cd8a4b9cfdf5c
Alexander Thomas <6ftDwarf@gmail.com>
2018-08-25 15:06:33 -0700

    Add switching between game modes

---
+++ Assets/Scripts/ModeSwitcher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModeSwitcher : MonoBehaviour {
+
+    public GameObject godCam;
+    public GameObject avatar;
+
+
+
+    private void Awake()
+    {
+        godCam.SetActive(true);
+        avatar.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            godCam.SetActive(!godCam.activeInHierarchy);
+            avatar.SetActive(!avatar.activeInHierarchy);
+        }
+    }
+
+}
