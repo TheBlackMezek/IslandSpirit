df4aaa63e4c52e4f713e143a5c2a8e36d6ff7382
Alexander Thomas <6ftDwarf@gmail.com>
2018-08-25 14:24:19 -0700

    Add basic terrain editing

--- Assets/Scripts/GodController.cs
+++ Assets/Scripts/GodController.cs
@@ -7,12 +7,38 @@ public class GodController : MonoBehaviour {
     public float lookSensitivity;
     public float moveSpeed;
 
+    public float terrainRaiseSpeed;
+
     private float xRotOffset = 0;
+    private Terrain terrain;
 
 
+
+    private void OnValidate()
+    {
+        terrain = GameObject.Find("Terrain").GetComponent<Terrain>();
+    }
 
     private void Update()
     {
+        if(Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray.origin, ray.direction, out hit, 10000f, LayerMask.GetMask("TerrainEdit")))
+            {
+                Vector3 v3pos = hit.point - terrain.transform.position;
+                v3pos.x /= terrain.terrainData.size.x;
+                v3pos.y /= terrain.terrainData.size.y;
+                v3pos.z /= terrain.terrainData.size.z;
+                Vector2 terrainPos = new Vector2(v3pos.x * terrain.terrainData.heightmapWidth,
+                                                 v3pos.z * terrain.terrainData.heightmapHeight);
+                float[,] heights = terrain.terrainData.GetHeights((int)terrainPos.x, (int)terrainPos.y, 1, 1);
+                heights[0, 0] += terrainRaiseSpeed * Time.deltaTime;
+                terrain.terrainData.SetHeights((int)terrainPos.x, (int)terrainPos.y, heights);
+            }
+        }
+
         if(Input.GetMouseButton(1))
         {
             Cursor.lockState = CursorLockMode.Locked;
---
+++ Assets/Scripts/TerrainResetter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainResetter : MonoBehaviour {
+
+    public float baseHeight;
+
+    private Terrain terrain;
+
+
+
+    private void OnValidate()
+    {
+        terrain = GetComponent<Terrain>();
+    }
+
+    private void Awake()
+    {
+        ResetHeights();
+    }
+
+    private void OnApplicationQuit()
+    {
+        ResetHeights();
+    }
+
+    private void ResetHeights()
+    {
+        float[,] heights = terrain.terrainData.GetHeights(0, 0, terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight);
+        for (int x = 0; x < terrain.terrainData.heightmapWidth; ++x)
+        {
+            for (int y = 0; y < terrain.terrainData.heightmapHeight; ++y)
+            {
+                heights[x, y] = baseHeight;
+            }
+        }
+        terrain.terrainData.SetHeights(0, 0, heights);
+    }
+
+}
