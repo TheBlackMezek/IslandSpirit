a48c485dcf07beb36a9c23edafa2b66e25be8bef
Alexander Thomas <6ftDwarf@gmail.com>
2018-08-25 13:51:42 -0700

    Add Cinemachine & god cam

---
+++ Assets/Scripts/GodController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GodController : MonoBehaviour {
+
+    public float lookSensitivity;
+    public float moveSpeed;
+
+    private float xRotOffset = 0;
+
+
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(1))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            transform.eulerAngles += Vector3.up * Input.GetAxis("Mouse X")
+                                   * lookSensitivity * Time.deltaTime;
+            xRotOffset += -Input.GetAxis("Mouse Y") * lookSensitivity * Time.deltaTime;
+            Mathf.Clamp(xRotOffset, -90f, 90f);
+            transform.eulerAngles = new Vector3(xRotOffset, transform.eulerAngles.y, transform.eulerAngles.z);
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        transform.position += transform.forward * Input.GetAxis("ForeBack") * Time.deltaTime * moveSpeed
+                            + transform.right * Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed
+                            + transform.up * Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+    }
+
+}
