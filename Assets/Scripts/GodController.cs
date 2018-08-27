f2829fe504cb0fa34d0d688a7493738b81b2dc5e
Alexander Thomas <6ftDwarf@gmail.com>
2018-08-27 12:51:31 -0700

    Add target circle for terrain editing

--- Assets/Scripts/GodController.cs
+++ Assets/Scripts/GodController.cs
@@ -10,37 +10,67 @@ public class GodController : MonoBehaviour {
     public float terrainRaiseSpeed;
     public float terrainRaiseRadius;
 
+    public float targetCircleFloatHeight;
+
     private float xRotOffset = 0;
     private Terrain terrain;
+    private Transform targetCircle;
 
 
 
     private void OnValidate()
     {
         terrain = GameObject.Find("Terrain").GetComponent<Terrain>();
+        targetCircle = transform.Find("TargetCircle");
     }
 
     private void Update()
     {
-        if(Input.GetMouseButton(0))
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        bool impact = Physics.Raycast(ray.origin, ray.direction, out hit, 10000f, LayerMask.GetMask("TerrainEdit"));
+
+        if (impact)
         {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if(Physics.Raycast(ray.origin, ray.direction, out hit, 10000f, LayerMask.GetMask("TerrainEdit")))
+            Vector3 v3pos = hit.point - terrain.transform.position;
+            v3pos.x /= terrain.terrainData.size.x;
+            v3pos.y /= terrain.terrainData.size.y;
+            v3pos.z /= terrain.terrainData.size.z;
+            Vector2 terrainPos = new Vector2(v3pos.x * terrain.terrainData.heightmapWidth,
+                                             v3pos.z * terrain.terrainData.heightmapHeight);
+
+            targetCircle.gameObject.SetActive(true);
+            float tHeight = terrain.SampleHeight(hit.point);
+            Debug.Log(tHeight);
+            targetCircle.position = new Vector3(hit.point.x, tHeight + targetCircleFloatHeight, hit.point.z);
+            targetCircle.eulerAngles = Vector3.zero;
+            targetCircle.localScale = new Vector3(terrainRaiseRadius, 1, terrainRaiseRadius);
+
+            if (Input.GetMouseButton(0))
             {
-                Vector3 v3pos = hit.point - terrain.transform.position;
-                v3pos.x /= terrain.terrainData.size.x;
-                v3pos.y /= terrain.terrainData.size.y;
-                v3pos.z /= terrain.terrainData.size.z;
-                Vector2 terrainPos = new Vector2(v3pos.x * terrain.terrainData.heightmapWidth,
-                                                 v3pos.z * terrain.terrainData.heightmapHeight);
-                //float[,] heights = terrain.terrainData.GetHeights((int)terrainPos.x, (int)terrainPos.y, 1, 1);
-                //heights[0, 0] += terrainRaiseSpeed * Time.deltaTime;
-                //terrain.terrainData.SetHeights((int)terrainPos.x, (int)terrainPos.y, heights);
                 RaiseTerrainCircleLerpBrush((int)terrainPos.x, (int)terrainPos.y, terrainRaiseRadius,
                                             Time.deltaTime * terrainRaiseSpeed);
             }
         }
+        else
+        {
+            targetCircle.gameObject.SetActive(false);
+        }
+
+        //if (Input.GetMouseButton(0))
+        //{
+        //    if(impact)
+        //    {
+        //        Vector3 v3pos = hit.point - terrain.transform.position;
+        //        v3pos.x /= terrain.terrainData.size.x;
+        //        v3pos.y /= terrain.terrainData.size.y;
+        //        v3pos.z /= terrain.terrainData.size.z;
+        //        Vector2 terrainPos = new Vector2(v3pos.x * terrain.terrainData.heightmapWidth,
+        //                                         v3pos.z * terrain.terrainData.heightmapHeight);
+        //        RaiseTerrainCircleLerpBrush((int)terrainPos.x, (int)terrainPos.y, terrainRaiseRadius,
+        //                                    Time.deltaTime * terrainRaiseSpeed);
+        //    }
+        //}
 
         if(Input.GetMouseButton(1))
         {
