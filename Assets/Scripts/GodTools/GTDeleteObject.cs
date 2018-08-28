e276935aa146cfc4db8576af5d551e471f58b8f7
Alexander Thomas <6ftDwarf@gmail.com>
2018-08-28 12:39:57 -0700

    Add object delete tool

---
+++ Assets/Scripts/GodTools/GTDeleteObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GTDeleteObject : GodToolAbstract {
+
+    public override void OnMouseHeld(int button, TerrainHitData data, float dt, float toolRadius, GameObject placablePrefab)
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray.origin, ray.direction, out hit, 10000f, LayerMask.GetMask("PlacableObject")))
+        {
+            Destroy(hit.collider.transform.root.gameObject);
+        }
+    }
+
+}
